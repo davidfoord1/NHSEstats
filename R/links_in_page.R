199ebfb1ca38514f72ff998d29e46653eeb87496
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-03-22 10:48:31 +0000

    Add links_in_page function
    Add rvest dependency to DESCRIPTION
    Add function file to R/
    Add documentation file to man/

---
+++ R/links_in_page.R
@@ -0,0 +1,22 @@
+#' Extract all links from a web page
+#'
+#' Parses a page using [rvest], pulling the href attribute value from all <a>
+#' elements in the page. Only keeps strings starting with "https" so ignores
+#' e.g. header links.
+#'
+#' @param url Web address to search for links
+#'
+#' @returns (chr) Vector of link strings
+#'
+#' @keywords internal
+links_in_page <- function(url) {
+  links <- url |>
+    rvest::read_html(url) |>
+    rvest::html_elements("a") |>
+    rvest::html_attr("href") |>
+    stats::na.omit()
+
+  is_https <- startsWith(links, "https://")
+
+  links[is_https]
+}

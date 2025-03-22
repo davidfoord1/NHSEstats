07751c178784f38a3f011018a40218b513c54fbe
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-03-22 15:14:16 +0000

    Create default_url.R

---
+++ R/default_url.R
@@ -0,0 +1,7 @@
+#' Base NHSE statistics url
+#'
+#' Expected to be used unless otherwise specified, this allows the config
+#' to just specify a path relative to the work areas page.
+#'
+#' @keywords internal
+default_url <- "https://www.england.nhs.uk/statistics/statistical-work-areas/"

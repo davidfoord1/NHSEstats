fbde162680caa9547b0e17f0ba7123cd8087f951
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-03-24 19:00:57 +0000

    Add download functionality
    Add curl package dependency. Combine curl download metadata with pre-constructed metadata.

--- R/nhs_download.R
+++ R/nhs_download.R
@@ -39,17 +39,57 @@ nhs_download <- function(area, dest = ".", skip_existing = TRUE) {
   metadata <- as.data.frame(do.call(rbind, flat_links))
 
   colnames(metadata) <- paste0("url_", seq_len(ncol(metadata)))
-  colnames(metadata)[ncol(metadata)] <- "dl_link"
+  colnames(metadata)[NCOL(metadata)] <- "dl_link"
+
+  # filter out duplicates
+  metadata <- metadata[!duplicated(metadata$dl_link), ]
 
   metadata$file_name <- basename(metadata$dl_link)
   metadata$dest_name <- file.path(dest, metadata$file_name)
 
   if (!dir.exists(dest)) dir.create(dest, recursive = TRUE)
 
-  metadata$pre_existing <- vapply(metadata$dest_name,
-                                  file.exists,
-                                  logical(1))
+  metadata$pre_existing <- file.exists(metadata$dest_name)
 
-  metadata
+  if (skip_existing) {
+    to_download <- metadata[!metadata$pre_existing, ]
+  } else {
+    to_download <- metadata
+  }
+
+  if (NROW(to_download) > 0L) {
+    dl_metadata <- curl::multi_download(
+      to_download$dl_link,
+      to_download$dest_name
+    )
+  } else {
+    # empty data.frame if there are none to download
+    # i.e. they have all already exist
+    dl_metadata <- data.frame(
+      url = character(),
+      success = logical(),
+      status_code = integer(),
+      resumefrom = numeric(),
+      destfile = character(),
+      error = character(),
+      type = character(),
+      modified = character(),
+      time = numeric(),
+      headers = I(list())
+    )
+  }
+
+  metadata <- merge(metadata,
+                    dl_metadata,
+                    all.x = TRUE,
+                    by.x = "dl_link",
+                    by.y = "url")
+
+  metadata[c("dl_link",
+             "dest_name",
+             "file_name",
+             "pre_existing",
+             "success",
+             "time")]
 }
 

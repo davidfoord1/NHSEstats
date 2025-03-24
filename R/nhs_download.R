5e6cebe7afc2e1c0c4e7fa70ab81fe7b6f866fc1
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2025-03-24 14:45:49 +0000

    Add ability to get download links for area arg

--- R/nhs_download.R
+++ R/nhs_download.R
@@ -9,11 +9,20 @@
 #' @return (data.frame) Download imformatio
 #'
 #' @export
-#'
-#' @examples
 nhs_download <- function(area, dest = ".", skip_existing = TRUE) {
   area_config <- load_yaml_config("areas.yaml")
+  area_config <- area_config[[area]]
 
-  area_config
+  url <- paste0(default_url, area_config$url)
+
+  dl_urls <- links_by_regex(url, area_config$patterns)
+  file_names = basename(dl_urls)
+  dest_file = file.path(dest, file_names)
+
+  meta <- data.frame(dl_urls,
+                     file_names,
+                     dest_file)
+
+  meta
 }
 
